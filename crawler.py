f68aec7cf07b330993e265d4675ce20e06eb62e9
lhs950204 <lhs950204@naver.com>
2022-09-08 23:13:01 +0900

    driver 관련 세팅 추가

--- crawler.py
+++ crawler.py
@@ -1,15 +1,29 @@
 from typing import Any
-import logging
-import selenium
-
+from typing_extensions import Literal
+from selenium import webdriver
 from utils import logger_wrapper
 
-logger = logging.getLogger()
+OS_TYPES = Literal["mac", "window", "m1", "linux"]
 
 
 class BaseCrawler(object):
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, os_type: OS_TYPES = "mac") -> None:
         self.url = url
+        self.driver = self.load_selenium(os_type)
+
+    def load_selenium(self, os_type: OS_TYPES) -> webdriver.Chrome:
+        if os_type == "mac":
+            driver_path = "./drivers/chromedriver_mac_64"
+        elif os_type == "window":
+            driver_path = "./drivers/chromedriver_win_32.exe"
+        elif os_type == "linux":
+            driver_path = "./drivers/chromedriver_mac_64"
+        else:
+            assert os_type == "m1", "올바르지 않은 os type 입니다."
+            driver_path = "./drivers/chromedriver_mac_m1"
+
+        driver = webdriver.Chrome(driver_path)
+        return driver
 
     @logger_wrapper
     def pre_process(self) -> Any:
